8a505190e2a1fd5305f3609280f7117eb9851338
Manuel Zavatta <manuel.zavatta@gmail.com>
2016-02-25 09:36:04 +0100

    Release 1.0.5

--- module.inc.php
+++ module.inc.php
@@ -3,7 +3,7 @@
 |* -[ Tasks - Module Information ]------------------------------------------- *|
 \* -------------------------------------------------------------------------- */
 $module_name="tasks";
-$module_version="1.0.4";
+$module_version="1.0.5";
 $module_title="Tasks";
 $module_description="Tasks and to-do lists";
 $module_create_menu=TRUE;
